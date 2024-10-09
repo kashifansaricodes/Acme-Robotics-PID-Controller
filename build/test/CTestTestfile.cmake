152b4999d6e61c8b027174ce010f0b57d6a36c92
Kashif Ansari <kansari@umd.edu>
2024-10-09 18:15:02 -0400

    added UML

--- build/test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kshitij/Acme-Robotics-PID-Controller/test
-# Build directory: /home/kshitij/Acme-Robotics-PID-Controller/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/kshitij/Acme-Robotics-PID-Controller/build/test/cpp-test[1]_include.cmake")
--- build/test/cpp-test[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/home/kshitij/Acme-Robotics-PID-Controller/build/test/cpp-test[1]_tests.cmake")
-  include("/home/kshitij/Acme-Robotics-PID-Controller/build/test/cpp-test[1]_tests.cmake")
-else()
-  add_test(cpp-test_NOT_BUILT cpp-test_NOT_BUILT)
-endif()
